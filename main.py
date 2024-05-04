b27d7848e0de8db290dd1cf54e72a3385d57dda7
Li Xing Yin <lixingyin@Lis-MacBook-Air-2.local>
2024-05-04 01:45:41 -0400

    init2

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
 
 # flask --app main run
